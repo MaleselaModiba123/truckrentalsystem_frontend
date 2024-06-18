184ac8ebc72687ceed8ca6bdaf749dc9131e3fab
MaleselaModiba123 <163680637+MaleselaModiba123@users.noreply.github.com>
2024-06-18 23:19:37 +0200

    Add files via upload

---
+++ truckrentalsystem_frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className='footer'>
+        <span>Copyright © 2024 Truck Rental System. All rights reserved. </span>
+      </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
